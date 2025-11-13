421f218eae72a72d24c2c3315689910c1e5c3116
yangjiandong <chunquedong@163.com>
2025-11-13 21:41:38 +0800

    update build file

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ sric gui/module.scm -fmake
 sric demo/module.scm
 
 fan fmake native/waseNanovg.fmake
+fan fmake native/wasePlutovg.fmake
+
 fan fmake native/demoWin.fmake
 fan fmake native/demoNanovg.fmake
--- build_debug.sh
+++ build_debug.sh
@@ -5,6 +5,7 @@ sric gui/module.scm -fmake -debug
 sric demo/module.scm
 
 fan fmake native/waseNanovg.fmake -debug
+fan fmake native/wasePlutovg.fmake -debug
 
 fan fmake native/demoWin.fmake -debug -G
 fan fmake native/demoNanovg.fmake -debug -G
